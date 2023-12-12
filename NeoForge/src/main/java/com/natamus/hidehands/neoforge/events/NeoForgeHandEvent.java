eca1c6ea8f5a5948dbaeea097593141aca7fe7ca
Rick South <rick@natamus.com>
2023-12-12 09:55:54 +0100

    Hide Hands source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/hidehands/neoforge/events/NeoForgeHandEvent.java
@@ -0,0 +1,18 @@
+package com.natamus.hidehands.neoforge.events;
+
+import com.natamus.hidehands.events.HandEvent;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.neoforge.client.event.RenderHandEvent;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber(value = Dist.CLIENT)
+public class NeoForgeHandEvent {
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	public static void onHandRender(RenderHandEvent e) {
+		if (!HandEvent.onHandRender(e.getHand(), e.getPoseStack(), e.getItemStack())) {
+			e.setCanceled(true);
+		}
+	}
+}
