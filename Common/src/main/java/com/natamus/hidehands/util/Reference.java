4b3d723ede472ee85937eab91e5de50460a9ba38
Rick South <rick@natamus.com>
2024-07-26 21:49:45 +0200

    (v4.3) Changed the default value of alwaysHideEmptyMainHand to false.

--- Common/src/main/java/com/natamus/hidehands/util/Reference.java
+++ Common/src/main/java/com/natamus/hidehands/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.hidehands.util;
 public class Reference {
 	public static final String MOD_ID = "hidehands";
 	public static final String NAME = "Hide Hands";
-	public static final String VERSION = "4.2";
+	public static final String VERSION = "4.3";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
