e20bcb2194f2ba876082ea8ee74433d99c059669
Rick South <rick@natamus.com>
2024-07-28 07:28:50 +0200

    (v4.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/hidehands/ModFabricClient.java
+++ Fabric/src/main/java/com/natamus/hidehands/ModFabricClient.java
@@ -5,12 +5,18 @@ import com.natamus.collective.fabric.callbacks.CollectiveRenderEvents;
 import com.natamus.hidehands.events.HandEvent;
 
 import net.fabricmc.api.ClientModInitializer;
+import com.natamus.hidehands.util.Reference;
+import com.natamus.collective.check.ShouldLoadCheck;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
 
 public class ModFabricClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() { 
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		registerEvents();
 	}
 	
