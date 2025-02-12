f57412ca0a5a8fb9464648bf692ddd3c55331e49
Rick South <rick@natamus.com>
2025-02-12 23:29:22 +0100

    (v4.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/hidehands/forge/events/ForgeHandEvent.java
+++ Forge/src/main/java/com/natamus/hidehands/forge/events/ForgeHandEvent.java
@@ -5,12 +5,10 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber(value = Dist.CLIENT)
 public class ForgeHandEvent {
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
-	public void onHandRender(RenderHandEvent e) {
+	public static void onHandRender(RenderHandEvent e) {
 		if (!HandEvent.onHandRender(e.getHand(), e.getPoseStack(), e.getItemStack())) {
 			e.setCanceled(true);
 		}
