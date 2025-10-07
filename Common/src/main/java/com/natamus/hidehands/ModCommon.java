9aa42a7ffece06f3c2403c32bd28988dee873c1c
Rick South <rick@natamus.com>
2025-10-08 01:32:19 +0200

    Hide Hands source code for 1.21.10.

---
+++ Common/src/main/java/com/natamus/hidehands/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.hidehands;
+
+import com.natamus.hidehands.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
