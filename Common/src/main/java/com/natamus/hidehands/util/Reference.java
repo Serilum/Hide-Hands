85fec38eae525cb80244c52492d7e17e322c6a63
Rick South <rick@natamus.com>
2026-04-10 02:53:00 +0200

    Hide Hands source code for 26.1.2.

---
+++ Common/src/main/java/com/natamus/hidehands/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.hidehands.util;
+
+public class Reference {
+	public static final String MOD_ID = "hidehands";
+	public static final String NAME = "Hide Hands";
+	public static final String VERSION = "4.6";
+	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+}
